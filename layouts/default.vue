3c0bf938231f069cee8649d84e82bc47d1803e42
Tom <tgclayton1@gmail.com>
2020-12-29 09:06:29 +1300

    modify default layout template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
+  <div class="md:flex flex-col w-screen h-screen content-center p-6">
+    <NavBar />
     <Nuxt />
+    <div hidden id="snipcart" data-api-key="YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0"></div>
   </div>
 </template>
 
